a48e223cfd3d0fa1f6178e035588cd5665677f00
Tarun Garg <152618274+tarun-garg22@users.noreply.github.com>
2023-12-11 13:50:36 +0000

    numbers and math methods

--- 01_Basics/07_nums_and_math.js
+++ 01_Basics/07_nums_and_math.js
@@ -1,8 +1,36 @@
 const score = 400;
-console.log(score);
+// console.log(score);
 
 const balance = new Number (399);
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const other_Number = 23.789;
+
+// console.log(other_Number.toPrecision(3))
+
+const hundred = 10000;
+// console.log(hundred.toLocaleString('en-IN'));
+// console.log(Number.MAX_SAFE_INTEGER)
+
+
+// ++++++++++++++++++++++++++++++++++++Math++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.7));
+// console.log(Math.min(4,67,7,6));
+// console.log(Math.max(4,67,7,6));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1)
+
+const  min =10;
+const max =20;
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min)
